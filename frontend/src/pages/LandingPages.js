ee69efa2f6e03599fc06f170c4e2b2ec8a86b8ed
Imam ariadi <gamerapin139@gmail.com>
2025-08-14 09:29:46 +0700

    server habis

--- frontend/src/pages/LandingPages.js
+++ frontend/src/pages/LandingPages.js
@@ -7,14 +7,39 @@ import Testimoni from "../components/landing/Testimoni";
 import Footer from "../components/landing/Footer";
 
 const LandingPages = () => (
-  <>
-    <AppNavbar />
-    <HeroSection />
-    <Fitur />
-    <Alasan />
-    <Testimoni />
-    <Footer />
-  </>
+ // Aktifkan bagian ini untuk menampilkan pesan server habis
+  <div style={styles.errorContainer}>
+    <h1 style={styles.errorMessage}>
+      Maaf server anda habis, silahkan hubungi penyedia server anda
+    </h1>
+  </div>
+
+  // Komentar bagian ini jika ingin menampilkan pesan server habis
+  // <>
+  //   <AppNavbar />
+  //   <HeroSection />
+  //   <Fitur />
+  //   <Alasan />
+  //   <Testimoni />
+  //   <Footer />
+  // </>
 );
+const styles = {
+  errorContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    backgroundColor: "#f8d7da",
+    color: "#721c24",
+    textAlign: "center",
+    padding: "0 20px",
+  },
+  errorMessage: {
+    fontSize: "1.5rem",
+    fontWeight: "bold",
+  },
+};
+
 
 export default LandingPages;
